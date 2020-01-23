f36ace2532c4137f0d4b5da8cc46511167a1212e
davidvadkerti <davidvadkerti@gmail.com>
2020-01-23 12:13:59 +0100

    customLogs to zero_doc context

--- CustomTools.tab/Batch.panel/customLogs.nobutton/script.py
+++ CustomTools.tab/Batch.panel/customLogs.nobutton/script.py
@@ -1,8 +1,8 @@
 # print("log files are here:")
 # print("L:\\customToolslogs")
 
+__context__ = 'zero-doc'
+
 import subprocess
 # subprocess.Popen(r'explorer /select,"L:\customToolslogs\"')
 subprocess.Popen(r'explorer /select,"L:\customToolslogs\doNotErase-pointer"')
-
-
