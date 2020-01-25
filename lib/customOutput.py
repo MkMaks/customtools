d833bef2d8c094ed5f5bd4cebf80e2791a9951be
davidvadkerti <davidvadkerti@gmail.com>
2020-01-25 16:38:15 +0100

    adding Critical warnings

--- lib/customOutput.py
+++ lib/customOutput.py
@@ -13,10 +13,16 @@ colors = 10*["#ffc299","#ff751a","#cc5200","#ff6666","#ffd480","#b33c00","#ff884
             "#4d4d4d","#fff0d9","#ffc299","#ff751a","#cc5200","#ff6666","#ffd480","#b33c00","#ff884d","#d9d9d9","#9988bb","#4d4d4d","#e97800","#a6c844",]
 
 # list of Warnings rated as critical
-criticalWarnings = ['Elements have duplicate "Type Mark" values','There are identical instances in the same place',
-    'Room Tag is outside of its Room','Multiple Rooms are in the same enclosed region','One element is completely inside another',
-    'Room is not in a properly enclosed region', 'Room separation line is slightly off axis and may cause inaccuracies',
-    'Area is not in a properly enclosed region']
+criticalWarnings = ['Elements have duplicate "Type Mark" values',
+    'There are identical instances in the same place',
+    'Room Tag is outside of its Room',
+    'Multiple Rooms are in the same enclosed region',
+    'Multiple Areas are in the same enclosed region',
+    'One element is completely inside another',
+    'Room is not in a properly enclosed region',
+    'Room separation line is slightly off axis and may cause inaccuracies',
+    'Area is not in a properly enclosed region',
+    "Rectangular opening doesn't cut its host",]
 
 
 # formating time in seconts to HHMMSS format
