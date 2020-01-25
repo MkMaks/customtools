c20049917f1cb995446ce3942d2698310264d6db
davidvadkerti <davidvadkerti@gmail.com>
2020-01-25 12:26:31 +0100

    CustomToolsInstall - fixing not working CSS setting by constructing file path using UserName system variable instead of %APPDATA%

--- updater/source/CustomToolsInstall.ps1
+++ updater/source/CustomToolsInstall.ps1
@@ -1,4 +1,12 @@
 pyrevit extend ui CustomTools https://bitbucket.org/davidvadkerti/customtools.git
 pyrevit configs logs none
 pyrevit configs colordocs enable
-pyrevit configs outputcss %APPDATA%\pyRevit\Extensions\CustomTools.extension\outputstylesCustom.css
+
+#constructing CSS file path since %APPDATA% is not working
+$firstPath = "C:\Users\"
+$secondPath = "\AppData\Roaming\pyRevit\Extensions\CustomTools.extension\outputstylesCustom.css"
+$fullPath = "$($firstPath)$($env:UserName)$($secondPath)"
+#Write-Output "$($fullPath)" > new.txt
+
+#setting CSS file
+pyrevit configs outputcss $fullPath
