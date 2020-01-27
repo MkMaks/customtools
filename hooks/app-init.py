f775de55fe05e9e006acac98c0180ff2938cc597
davidvadkerti <davidvadkerti@gmail.com>
2020-01-27 11:51:01 +0100

    new app-init script to log CustomTools version to server

---
+++ hooks/app-init.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+
+# from pyrevit import EXEC_PARAMS
+# from pyrevit import forms, script
+from hooksScripts import hooksLogger, versionLogger, releasedVersion, snapshot
+
+versionLogger(releasedVersion,snapshot)
