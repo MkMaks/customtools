3b4ef67ca198280cef9ab6db12ab911f36d01f90
davidvadkerti <davidvadkerti@gmail.com>
2020-01-28 20:25:06 +0100

    implementing option to not show dialog box when config file = 0

--- hooks/command-before-exec[ID_SETTINGS_PROJECT_PARAMETERS].py
+++ hooks/command-before-exec[ID_SETTINGS_PROJECT_PARAMETERS].py
@@ -2,23 +2,39 @@
 from pyrevit import EXEC_PARAMS
 from pyrevit import forms, script
 
-res = forms.alert("POZOR!\n\n"
-	            "Pri vytváraní nového Shared Parametru si najskôr pozrite, či už podobný parameter nie je vytvorený.",
-				title="Project Parameters",
-				footer="CustomTools Hooks",
-                options=["Zobraziť zoznam Shared Parametrov na Wiki",
-                         "Pridať Parameter",
-                         "Zrušiť"])
-if res  == "Pridať Parameter":
-	EXEC_PARAMS.event_args.Cancel = False
-	# logging to server
-	from hooksScripts import hooksLogger
-	hooksLogger("Project or Shared Parameters")
-elif res  == "Zrušiť":
-	EXEC_PARAMS.event_args.Cancel = True
-elif res  == "Zobraziť zoznam Shared Parametrov na Wiki":
-	EXEC_PARAMS.event_args.Cancel = True
-	url = 'https://gfi.miraheze.org/wiki/Shared_parametre_pre_Revit'
-	script.open_url(url)
-else:
-	EXEC_PARAMS.event_args.Cancel = True
+# showing of dialog box with warning
+def dialogBox():
+	res = forms.alert("POZOR!\n\n"
+		            "Pri vytváraní nového Shared Parametru si najskôr pozrite, či už podobný parameter nie je vytvorený. Nepoužívajte diakritiku.",
+					title="Project Parameters",
+					footer="CustomTools Hooks",
+	                options=["Zobraziť zoznam Shared Parametrov na Wiki",
+	                         "Pridať Parameter",
+	                         "Zrušiť"])
+	if res  == "Pridať Parameter":
+		EXEC_PARAMS.event_args.Cancel = False
+		# logging to server
+		from hooksScripts import hooksLogger
+		hooksLogger("Project or Shared Parameters")
+	elif res  == "Zrušiť":
+		EXEC_PARAMS.event_args.Cancel = True
+	elif res  == "Zobraziť zoznam Shared Parametrov na Wiki":
+		EXEC_PARAMS.event_args.Cancel = True
+		url = 'https://gfi.miraheze.org/wiki/Shared_parametre_pre_Revit'
+		script.open_url(url)
+	else:
+		EXEC_PARAMS.event_args.Cancel = True
+
+# try to find config file for people who dont want to see the hook
+try:
+	configFile = open("C:\\PyrevitExtensions\\CustomTools\\hooksConfig.txt","r")
+	configSettingsList = (currentVersionFile.readlines())
+	configSettings = str(currentVersionList)[2:-2]
+	# if content not equal to zero show the dialog box
+	if configSettings == "0":
+		pass
+	else:
+		dialogBox()
+# if file not found show the dialog box
+except:
+	dialogBox()
