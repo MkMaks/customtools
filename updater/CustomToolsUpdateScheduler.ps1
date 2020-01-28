dc1ecaa1170c4584c4ef70afc845531547a8bb17
davidvadkerti <davidvadkerti@gmail.com>
2020-01-28 18:44:16 +0100

    update purge

--- updater/CustomToolsUpdateScheduler.ps1
+++ updater/CustomToolsUpdateScheduler.ps1
@@ -2,10 +2,8 @@
 $PathStartup = "C:\Users\$env:UserName\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 
 #constructing file path since %APPDATA% is not working
-#$PathUpdater = "C:\Users\$($env:UserName)\AppData\Roaming\pyRevit\Extensions\CustomTools.extension\updater\CustomToolsUpdater.ps1"
 $PathUpdaterLink = "C:\Users\$($env:UserName)\AppData\Roaming\pyRevit\Extensions\CustomTools.extension\updater\CustomToolsUpdater.cmd"
 
-#Copy-Item -Path $PathUpdater -Destination $PathStartup
 Copy-Item -Path $PathUpdaterLink -Destination $PathStartup
 
 #Write-Output "$PathUpdater $PathUpdaterLink $PathStartup" > new.txt
