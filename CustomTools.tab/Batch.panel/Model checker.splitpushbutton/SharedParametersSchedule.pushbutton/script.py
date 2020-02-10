7682e36a0a511f6d5cb34822647df168575f8cc7
davidvadkerti <davidvadkerti@gmail.com>
2020-02-10 12:47:25 +0100

    changed URL for sharedparameters config file to U:/

--- CustomTools.tab/Batch.panel/Model checker.splitpushbutton/SharedParametersSchedule.pushbutton/script.py
+++ CustomTools.tab/Batch.panel/Model checker.splitpushbutton/SharedParametersSchedule.pushbutton/script.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 __title__ = 'Shared Parameter Schedule'
 __author__ = 'David Vadkerti'
-__doc__ = 'Lists all Shared Parameters'
+__doc__ = 'Lists all Shared Parameters from text config file'
 
 from pyrevit import script
 from pyrevit import output
@@ -16,53 +16,53 @@ output = script.get_output()
 output.print_md("# SHARED PARAMETERS SCHEDULE")
 
 def parseSharedParamFile(filePath,encodingType):
-	scheduleData = []
-	f = io.open(filePath, mode="r", encoding=encodingType)
-	# f = open("L:\\REVIT\\Kniznica_REVIT\\4_Parametre\\forCustomTools\\GFI-shared-parametre.txt", "rU", encoding="utf-8")
+    scheduleData = []
+    f = io.open(filePath, mode="r", encoding=encodingType)
 
-	groupDict = {}
-	groupNo = 1
-	for line in f:
-	    # Creating Parameter group dictionary
-	    diff = line[0:5]
-	    if diff =="GROUP":
-	        # deleting GROUP in the begining
-	        strippedGroup = line[6:]
-	        # name is after Tab
-	        groupName = strippedGroup.split("\t")[1]
-	        # cutting ending \n sign in each groupName end
-	        groupName = groupName[:-1]
-	        groupDict[str(groupNo)] = groupName
-	        groupNo += 1
-	    elif diff == "PARAM":
-	        strippedParam = line[5:]        
-	        # split to list by columns - tabs
-	        paramPartList = line.split("\t")
-	        # param name is in 3rd column
-	        paramName = paramPartList[2]
-	        paramDataType = paramPartList[3]
-	        paramGroupNo = str(paramPartList[5])
-	        paramGroupName = groupDict.get(paramGroupNo)
-	        paramDescription = paramPartList[7][:200]
-	        paramList = [paramName, paramDataType, paramGroupName, paramDescription]
+    groupDict = {}
+    groupNo = 1
+    for line in f:
+        # Creating Parameter group dictionary
+        diff = line[0:5]
+        if diff =="GROUP":
+            # deleting GROUP in the begining
+            strippedGroup = line[6:]
+            # name is after Tab
+            groupName = strippedGroup.split("\t")[1]
+            # cutting ending \n sign in each groupName end
+            groupName = groupName[:-1]
+            groupDict[str(groupNo)] = groupName
+            groupNo += 1
+        elif diff == "PARAM":
+            strippedParam = line[5:]        
+            # split to list by columns - tabs
+            paramPartList = line.split("\t")
+            # param name is in 3rd column
+            paramName = paramPartList[2]
+            paramDataType = paramPartList[3]
+            paramGroupNo = str(paramPartList[5])
+            paramGroupName = groupDict.get(paramGroupNo)
+            paramDescription = paramPartList[7][:200]
+            paramList = [paramName, paramDataType, paramGroupName, paramDescription]
 
-	        scheduleData.append(paramList)
+            scheduleData.append(paramList)
 
-	output.print_table(table_data=scheduleData,
-	                   title = filePath,
-	                   columns=["Name", "Data Type", "Group", "Description"],
-	                   formats=['', '', '', ''])
+    output.print_table(table_data=scheduleData,
+                       title = filePath,
+                       columns=["Name", "Data Type", "Group", "Description"],
+                       formats=['', '', '', ''])
 
-	# for timing------
-	endtime = timer.get_time()
-	print(hmsTimer(endtime))
+    # for timing------
+    endtime = timer.get_time()
+    print(hmsTimer(endtime))
 
 
-filePath = "L:\\REVIT\\Kniznica_REVIT\\4_Parametre\\GFI-shared-parametre.txt"
+filePath = "U:\\REVIT\\Kniznica_REVIT\\4_Parametre\\GFI-shared-parametre.txt"
+
 try:
-	parseSharedParamFile(filePath,"utf-16")
+    parseSharedParamFile(filePath,"utf-16")
 except:
-	try:
-		parseSharedParamFile(filePath,"utf-8")
-	except:
-    	print("Source file should be saved with unicode UTF-16 encoding.")
+    try:
+        parseSharedParamFile(filePath,"utf-8")
+    except:
+        print("Source file should be saved with unicode UTF-16 encoding.")
