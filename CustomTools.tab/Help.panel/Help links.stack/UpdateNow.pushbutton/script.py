a40a9d2716b4d8a26bc1d833ee364949b94942bc
davidvadkerti <davidvadkerti@gmail.com>
2020-02-17 11:02:57 +0100

    fixing building of updater path

--- CustomTools.tab/Help.panel/Help links.stack/UpdateNow.pushbutton/script.py
+++ CustomTools.tab/Help.panel/Help links.stack/UpdateNow.pushbutton/script.py
@@ -6,13 +6,15 @@ __context__ = 'zero-doc'
 __doc__ = 'Manual instant update of CustomTools without system restart.' \
           'Click Reload button after installation.' \
           'CustomTools checks for updates on system startup.'
-__highlight__ = 'new'
 
-# import getpass
-# username = getpass.getuser()
-# print(username)
+import getpass
+username = getpass.getuser()
 
 import subprocess
-# running CustomToolsUpdater.cmd script at:
-# C:\Users\pxls\Documents\customtools\updater\CustomToolsUpdater.cmd
-p = subprocess.Popen(['..\\..\\..\\..\\updater\\CustomToolsUpdater.cmd'])
+try:
+	# running CustomToolsUpdater.cmd script at:
+	# %AppData%\\Roaming\\pyRevit\\Extensions\\CustomTools.extension\\updater\\CustomToolsUpdater.cmd
+	p = subprocess.Popen(['C:\\Users\\'+username+'\\AppData\\Roaming\\pyRevit\\Extensions\\CustomTools.extension\\updater\\CustomToolsUpdater.cmd'])
+	# p = subprocess.Popen(['..\\..\\..\\..\\updater\\CustomToolsUpdater.cmd'])
+except:
+	print("Something went wrong. Install manualy at %AppData%\\Roaming\\pyRevit\\Extensions\\CustomTools.extension\\updater\\CustomToolsUpdater.cmd")
