d69c8ff526b7d77317d764aa532bb934c7ae2078
davidvadkerti <davidvadkerti@gmail.com>
2020-02-18 11:59:38 +0100

    appdata variable instead of path constructing

--- CustomTools.tab/Help.panel/Help links.stack/UpdateNow.pushbutton/script.py
+++ CustomTools.tab/Help.panel/Help links.stack/UpdateNow.pushbutton/script.py
@@ -7,14 +7,13 @@ __doc__ = 'Manual instant update of CustomTools without system restart.' \
           'Click Reload button after installation.' \
           'CustomTools checks for updates on system startup.'
 
-import getpass
-username = getpass.getuser()
-
+import os
 import subprocess
 try:
 	# running CustomToolsUpdater.cmd script at:
 	# %AppData%\\Roaming\\pyRevit\\Extensions\\CustomTools.extension\\updater\\CustomToolsUpdater.cmd
-	p = subprocess.Popen(['C:\\Users\\'+username+'\\AppData\\Roaming\\pyRevit\\Extensions\\CustomTools.extension\\updater\\CustomToolsUpdater.cmd'])
-	# p = subprocess.Popen(['..\\..\\..\\..\\updater\\CustomToolsUpdater.cmd'])
+	appdataPath = os.getenv('APPDATA')
+	updaterPath = appdataPath + '\\pyRevit\\Extensions\\CustomTools.extension\\updater\\CustomToolsUpdater.cmd'
+	p = subprocess.Popen([updaterPath])
 except:
 	print("Something went wrong. Install manualy at %AppData%\\Roaming\\pyRevit\\Extensions\\CustomTools.extension\\updater\\CustomToolsUpdater.cmd")
