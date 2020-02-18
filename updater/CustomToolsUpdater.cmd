ebfc61374890ec3c3279448a952a3ceafb9991e3
davidvadkerti <davidvadkerti@gmail.com>
2020-02-18 18:53:13 +0100

    rocketmode enabled for updater

--- updater/CustomToolsUpdater.cmd
+++ updater/CustomToolsUpdater.cmd
@@ -1,2 +1,3 @@
 PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
 PowerShell pyrevit extensions update CustomTools
+PowerShell pyrevit configs rocketmode enable
