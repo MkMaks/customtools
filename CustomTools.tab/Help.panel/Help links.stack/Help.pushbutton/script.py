5a01551d0c550538bcc1426898873b99fe7b249f
davidvadkerti <davidvadkerti@gmail.com>
2020-05-20 19:08:29 +0200

    Help - youtube playlist added

--- CustomTools.tab/Help.panel/Help links.stack/Help.pushbutton/script.py
+++ CustomTools.tab/Help.panel/Help links.stack/Help.pushbutton/script.py
@@ -1,9 +1,6 @@
-"""Opens help for the CustomTools Extension on GFI wiki website."""
 from pyrevit import script
 
-
 __context__ = 'zero-doc'
-
 
 url = 'https://gfi.miraheze.org/wiki/CustomTools_(Extension_pre_pyRevit)'
 script.open_url(url)
