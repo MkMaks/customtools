37a302a2ac3e6029eea65bf56d7a76a2b60816cf
davidvadkerti <davidvadkerti@gmail.com>
2020-05-30 22:45:00 +0200

    doc-closing hook - fixed unnecessary appearing when closing families

--- hooks/doc-closing.py
+++ hooks/doc-closing.py
@@ -2,51 +2,52 @@
 from pyrevit import EXEC_PARAMS
 from pyrevit import forms
 
-res = forms.alert("Chceš uložiť zoznam otvorených Views?",
-                  options=["Uložiť",
-                           "Neuložiť",
-                           "Zistiť viac"],
-                  title="Save List Of Opened Views",
-                  footer="CustomTools Hooks")
-if res  == "Uložiť":
-    # opened views
-    from Autodesk.Revit.UI import UIDocument
-    from Autodesk.Revit.DB.Document import GetElement
-    from pyrevit import script
+from Autodesk.Revit.UI import UIDocument
+from Autodesk.Revit.DB.Document import GetElement
+from pyrevit import script
 
-    doc = EXEC_PARAMS.event_args.Document
-    uidoc = UIDocument(doc)
-    docName = doc.PathName
+doc = EXEC_PARAMS.event_args.Document
+uidoc = UIDocument(doc)
+docName = doc.PathName
+fileExtension = docName[-3:]
 
-    output = script.get_output()
-    output.print_md("## List of Views for file: " + docName)
+if fileExtension == "rvt":
+    res = forms.alert("Chceš uložiť zoznam otvorených Views?",
+                      options=["Uložiť",
+                               "Neuložiť",
+                               "Zistiť viac"],
+                      title="Save List Of Opened Views",
+                      footer="CustomTools Hooks")
+    if res  == "Uložiť":
+        # opened views
+        output = script.get_output()
+        output.print_md("## List of Views for file: " + docName)
 
-    openUIviews = uidoc.GetOpenUIViews()
+        openUIviews = uidoc.GetOpenUIViews()
 
-    count = 0
-    scheduleData = []
-    allViewIds = []
-    for uiview in openUIviews:
-        count += 1
-        view_id = uiview.ViewId
-        viewName = doc.GetElement(view_id).Name
-        paramList = [str(count), viewName, output.linkify(view_id)]
-        scheduleData.append(paramList)
-        allViewIds.append(view_id)
+        count = 0
+        scheduleData = []
+        allViewIds = []
+        for uiview in openUIviews:
+            count += 1
+            view_id = uiview.ViewId
+            viewName = doc.GetElement(view_id).Name
+            paramList = [str(count), viewName, output.linkify(view_id)]
+            scheduleData.append(paramList)
+            allViewIds.append(view_id)
 
-    output.print_table(table_data=scheduleData,
-                       columns=["Number", "View Name", "View Id"],
-                       formats=['', '', ''])
+        output.print_table(table_data=scheduleData,
+                           columns=["Number", "View Name", "View Id"],
+                           formats=['', '', ''])
 
-    print("\nSave this html file on your drive.")
-    print("Use Import Report tool for reopening views.")
+        print("\nSave this html file on your drive.")
+        print("Use Import Report tool for reopening views.")
 
-elif res  == "Neuložiť":
-    pass
-elif res  == "Zistiť viac":
-    from pyrevit import script
-    url = 'https://youtu.be/1lANcq6WONI'
-    script.open_url(url)
-else:
-    pass
-
+    elif res  == "Neuložiť":
+        pass
+    elif res  == "Zistiť viac":
+        from pyrevit import script
+        url = 'https://youtu.be/1lANcq6WONI'
+        script.open_url(url)
+    else:
+        pass
