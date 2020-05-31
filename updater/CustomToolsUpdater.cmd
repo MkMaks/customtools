4b30018d60f3d390b5265cb3c854f3a41b07d806
davidvadkerti <davidvadkerti@gmail.com>
2020-05-31 11:18:19 +0200

    option for pyRevit as a git repo update

--- updater/CustomToolsUpdater.cmd
+++ updater/CustomToolsUpdater.cmd
@@ -1,3 +1,5 @@
 PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
 PowerShell pyrevit extensions update CustomTools
 PowerShell pyrevit configs rocketmode enable
+# when pyrevit update needed
+# pyrevit clones update master
