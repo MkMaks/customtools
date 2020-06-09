1cfe2cb69dcc5e45d2aa2a6d2ed38ff308b45057
davidvadkerti <davidvadkerti@gmail.com>
2020-06-10 00:40:15 +0200

    comments

--- updater/CustomToolsUpdater.cmd
+++ updater/CustomToolsUpdater.cmd
@@ -1,5 +1,5 @@
 PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
 PowerShell pyrevit extensions update CustomTools
 PowerShell pyrevit configs rocketmode enable
-# when pyrevit update needed
-# pyrevit clones update master
+rem when pyrevit update needed
+rem PowerShell pyrevit clones update master
