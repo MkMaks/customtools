128755d1e00138b454147401bd089a7cad37e4ca
davidvadkerti <davidvadkerti@gmail.com>
2020-06-10 21:25:08 +0200

    app-init auto update pyRevit settings added

--- hooks/app-init.py
+++ hooks/app-init.py
@@ -1,8 +1,18 @@
 # -*- coding: UTF-8 -*-
-
 from hooksScripts import versionLogger, releasedVersion, snapshot
 
 versionLogger(releasedVersion,snapshot)
+
+import os
+import subprocess
+try:
+    # running CustomToolsUpdater.cmd script at:
+    # %AppData%\\Roaming\\pyRevit\\Extensions\\CustomTools.extension\\updater\\CustomToolsUpdater.cmd
+    appdataPath = os.getenv('APPDATA')
+    updaterPath = appdataPath + '\\pyRevit\\Extensions\\CustomTools.extension\\updater\\CustomToolsUpdater.cmd'
+    p = subprocess.Popen([updaterPath])
+except:
+    pass
 
 # import os
 # import subprocess
