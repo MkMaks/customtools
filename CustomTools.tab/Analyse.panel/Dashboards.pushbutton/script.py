e220cf33e46022c3a4dca318a018b9d7db702f72
davidvadkerti <davidvadkerti@gmail.com>
2020-06-28 14:18:52 +0200

    New Dashboards feature - opens dashboards in Power BI

---
+++ CustomTools.tab/Analyse.panel/Dashboards.pushbutton/script.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+
+__title__ = 'Dashboards'
+__doc__ = '''Power BI Dashboards for analysing RVT model quality, CustomTools Hooks logs and more.
+
+You need to install Power BI Desktop application first.'''
+__author__ = 'David Vadkerti'
+__helpurl__ = 'https://gfi.miraheze.org/wiki/Power_BI'
+__highlight__= 'new'
+__context__ = 'zero-doc'
+
+# import subprocess
+# subprocess.Popen(r'explorer /select,"L:\powerBI\doNotErase-pointer"')
+from pyrevit import script
+
+output = script.get_output()
+
+url = 'L:\\powerBI\\'
+output.open_page(url)
