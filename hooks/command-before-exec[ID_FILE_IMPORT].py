f66a6f812a97090966bee02d7536c79218669814
davidvadkerti <davidvadkerti@gmail.com>
2020-10-30 18:52:29 +0100

    added turn off option

--- hooks/command-before-exec[ID_FILE_IMPORT].py
+++ hooks/command-before-exec[ID_FILE_IMPORT].py
@@ -2,34 +2,38 @@
 
 from pyrevit import EXEC_PARAMS
 from pyrevit import forms, script
+from hooksScripts import hookTurnOff
 
 doc = __revit__.ActiveUIDocument.Document
 docName = doc.PathName
 fileExtension = docName[-3:]
 
-# for not showing the hook in families - not saved families have no extension
-if fileExtension == "rvt" or fileExtension == "rte":
-   res = forms.alert("POZOR!\n\n"
-                    "Importovať CAD súbory by si mal len výnimočne.\n"
-                     "Nikdy neimportuj CAD priamo do modelu, ale do čistého RVT súboru.\n\n"
-                     "Si si istý, že vieš, čo robíš?",
-                     title="Import CAD file",
-                     footer="CustomTools Hooks",
-                     options=["Importovať",
-                              "Zrušiť",
-                              "Viac info o Importovaní CAD súborov"])
+def dialogBox():
+   # for not showing the hook in families - not saved families have no extension
+   if fileExtension == "rvt" or fileExtension == "rte":
+      res = forms.alert("POZOR!\n\n"
+                       "Importovať CAD súbory by si mal len výnimočne.\n"
+                        "Nikdy neimportuj CAD priamo do modelu, ale do čistého RVT súboru.\n\n"
+                        "Si si istý, že vieš, čo robíš?",
+                        title="Import CAD file",
+                        footer="CustomTools Hooks",
+                        options=["Importovať",
+                                 "Zrušiť",
+                                 "Viac info o Importovaní CAD súborov"])
 
-   if res  == "Importovať":
-      EXEC_PARAMS.event_args.Cancel = False
-      # logging to server
-      from hooksScripts import hooksLogger
-      hooksLogger("CAD file import", doc)
+      if res  == "Importovať":
+         EXEC_PARAMS.event_args.Cancel = False
+         # logging to server
+         from hooksScripts import hooksLogger
+         hooksLogger("CAD file import", doc)
 
-   elif res  == "Zrušiť":
-      EXEC_PARAMS.event_args.Cancel = True
-   elif res  == "Viac info o Importovaní CAD súborov":
-      EXEC_PARAMS.event_args.Cancel = True
-      url = 'https://gfi.miraheze.org/wiki/Postupy,_ktor%C3%BDm_je_potrebn%C3%A9_sa_vyhn%C3%BA%C5%A5_-_Revit#Importovanie_DWG'
-      script.open_url(url)
-   else:
-      EXEC_PARAMS.event_args.Cancel = True
+      elif res  == "Zrušiť":
+         EXEC_PARAMS.event_args.Cancel = True
+      elif res  == "Viac info o Importovaní CAD súborov":
+         EXEC_PARAMS.event_args.Cancel = True
+         url = 'https://gfi.miraheze.org/wiki/Postupy,_ktor%C3%BDm_je_potrebn%C3%A9_sa_vyhn%C3%BA%C5%A5_-_Revit#Importovanie_DWG'
+         script.open_url(url)
+      else:
+         EXEC_PARAMS.event_args.Cancel = True
+
+hookTurnOff(dialogBox,2)
--- hooks/command-before-exec[ID_UNLOCK_ELEMENTS].py
+++ hooks/command-before-exec[ID_UNLOCK_ELEMENTS].py
@@ -2,23 +2,27 @@
 
 from pyrevit import EXEC_PARAMS
 from pyrevit import forms, script
+from hooksScripts import hookTurnOff
 
-res = forms.alert("POZOR!\n\n"
-	              "Chceš naozaj odpinovať tento element?\n"
-                  "Elementy sú väčšinou zapinované kvôli tomu, aby nimi náhodou niekto nepohol.\n\n"
-                  "Chceš to naozaj urobiť?",
-                  title="Unpin element",
-                  footer="CustomTools Hooks",
-                  options=["Unpin",
-                           "Zrušiť",
-                           "Viac info o Pin/Unpin"])
-if res  == "Unpin":
-	EXEC_PARAMS.event_args.Cancel = False
-elif res  == "Zrušiť":
-	EXEC_PARAMS.event_args.Cancel = True
-elif res  == "Viac info o Pin/Unpin":
-	EXEC_PARAMS.event_args.Cancel = True
-	url = 'https://gfi.miraheze.org/w/index.php?search=unpin&title=Špeciálne%3AHľadanie&go=Ísť+na'
-	script.open_url(url)
-else:
-	EXEC_PARAMS.event_args.Cancel = True
+def dialogBox():
+    res = forms.alert("POZOR!\n\n"
+    	              "Chceš naozaj odpinovať tento element?\n"
+                      "Elementy sú väčšinou zapinované kvôli tomu, aby nimi náhodou niekto nepohol.\n\n"
+                      "Chceš to naozaj urobiť?",
+                      title="Unpin element",
+                      footer="CustomTools Hooks",
+                      options=["Unpin",
+                               "Zrušiť",
+                               "Viac info o Pin/Unpin"])
+    if res  == "Unpin":
+    	EXEC_PARAMS.event_args.Cancel = False
+    elif res  == "Zrušiť":
+    	EXEC_PARAMS.event_args.Cancel = True
+    elif res  == "Viac info o Pin/Unpin":
+    	EXEC_PARAMS.event_args.Cancel = True
+    	url = 'https://gfi.miraheze.org/w/index.php?search=unpin&title=Špeciálne%3AHľadanie&go=Ísť+na'
+    	script.open_url(url)
+    else:
+    	EXEC_PARAMS.event_args.Cancel = True
+
+hookTurnOff(dialogBox,10)    
