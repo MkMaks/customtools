b1d0857957368069d8ad626510c127adfab63158
davidvadkerti <davidvadkerti@gmail.com>
2020-11-11 17:06:00 +0100

    first atempt to fix an error if view is already on a sheet

--- CustomTools.tab/Tools.panel/Query1.stack/Place View on Sheet.pushbutton/script.py
+++ CustomTools.tab/Tools.panel/Query1.stack/Place View on Sheet.pushbutton/script.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 __doc__ = 'Places selected opened View or Schedule on active Sheet. ' \
           'Activate sheet first.'
 __title__ = 'Place View On Sheet'
@@ -5,8 +6,7 @@ __helpurl__ = 'https://youtu.be/_DVTvM8VVAw'
 
 
 from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, Transaction, XYZ, Viewport, ScheduleSheetInstance
-from pyrevit import revit, DB, coreutils, forms
-# from pyrevit import revit, coreutils, forms
+from pyrevit import revit, DB, coreutils, forms, script
 
 
 doc = __revit__.ActiveUIDocument.Document
@@ -18,21 +18,42 @@ loc = XYZ(0,0,0)
 
 
 def placingSchedule(selectedSheet):
-	if str(selectedSheet.ViewType) == "DrawingSheet":
-		selectedSheetId = selectedSheet.Id
-		curview = revit.active_view
-		if str(curview.ViewType) == "Schedule":
-			t = Transaction(doc, "Place schedule on active Sheet")
-			t.Start()
-			ScheduleSheetInstance.Create(doc, selectedSheetId, curview.Id, loc)
-			t.Commit()
-		else:
-			t = Transaction(doc, "Place view on active Sheet")
-			t.Start()
-			Viewport.Create(doc, selectedSheetId, curview.Id, loc)
-			t.Commit()
-	else:
-		print("Activate sheet at first.")
+    if str(selectedSheet.ViewType) == "DrawingSheet":
+        selectedSheetId = selectedSheet.Id
+        curview = doc.ActiveView
+        try:
+	        if str(curview.ViewType) == "Schedule":
+	            t = Transaction(doc, "Place schedule on active Sheet")
+	            t.Start()
+	            ScheduleSheetInstance.Create(doc, selectedSheetId, curview.Id, loc)
+	            t.Commit()
+	        else:
+	            t = Transaction(doc, "Place view on active Sheet")
+	            t.Start()
+	            Viewport.Create(doc, selectedSheetId, curview.Id, loc)
+	            t.Commit()
+        except:
+        	# if view is already on the sheet
+            viewports_collector = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Viewports) \
+            	.WhereElementIsNotElementType().ToElements()
+
+            output = script.get_output()
+            md_schedule = "| Number | Sheet Name | Owner Sheet |\n| ----------- | ----------- | ----------- |"  
+            viewsOnSheet = []
+            count = 0
+            for viewport in viewports_collector:
+                view_id = viewport.ViewId
+                if curview.Id == view_id:
+                    count += 1
+                    sheet_id = viewport.SheetId
+                    sheetName = doc.GetElement(sheet_id).Name
+                    newScheduleLine = " \n| "+str(count)+" | "+sheetName+" | "+output.linkify(sheet_id)+" |"
+                    md_schedule += newScheduleLine
+            if count > 0:
+                output.print_md("# ACTIVE VIEW IS ALREADY ON THIS VIEW")
+                output.print_md(md_schedule)
+    else:
+        print("Activate sheet at first.")
 
 
 with forms.WarningBar(title='Vyber element vo View:'):
