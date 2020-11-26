717c18cf5e75bdbe2e1a4ccaaa5fdc02ddc6ac99
davidvadkerti <davidvadkerti@gmail.com>
2020-11-26 15:53:32 +0100

    changes in bundles

--- CustomTools.tab/Help.panel/BIM troubleshooting.pulldown/PostIssue.pushbutton/script.py
+++ CustomTools.tab/Help.panel/BIM troubleshooting.pulldown/PostIssue.pushbutton/script.py
@@ -1,6 +1,9 @@
-"""Opens the GFI BIM trouble shooting from for posting issues."""
+"""Opens the GFI BIM trouble shooting for posting issues."""
 from pyrevit import script
 
+__title__ = 'Post Issue'
+__doc__ = '''Opens the GFI BIM troubleshooting form for posting BIM related issues.'''
+__author__ = 'David Vadkerti'
 __context__ = 'zero-doc'
 
 url = 'https://airtable.com/shrTPnWkptt5zkBts'
--- CustomTools.tab/Help.panel/BIM troubleshooting.pulldown/ViewIssues.pushbutton/script.py
+++ CustomTools.tab/Help.panel/BIM troubleshooting.pulldown/ViewIssues.pushbutton/script.py
@@ -1,6 +1,9 @@
-"""Opens the GFI BIM trouble shooting from for posting issues."""
+"""Opens the GFI BIM trouble shooting for View issue tracker history."""
 from pyrevit import script
 
+__title__ = 'View Issues'
+__doc__ = '''Opens the GFI BIM troubleshooting history.'''
+__author__ = 'David Vadkerti'
 __context__ = 'zero-doc'
 
 url = 'https://airtable.com/shr8HrKsT7igtx68O/tblEH2vhKin56Q8kM'
