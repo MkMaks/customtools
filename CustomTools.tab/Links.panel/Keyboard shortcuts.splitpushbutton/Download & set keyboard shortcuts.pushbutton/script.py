3a2bdae1da08757237954bff13ac8d529676f724
davidvadkerti <davidvadkerti@gmail.com>
2021-04-15 20:57:30 +0200

    keyboard shortcuts leads to aritable base instead of wiki Download keyboard shortcuts leads to xml file inside the extension

--- CustomTools.tab/Links.panel/Keyboard shortcuts.splitpushbutton/Download & set keyboard shortcuts.pushbutton/script.py
+++ CustomTools.tab/Links.panel/Keyboard shortcuts.splitpushbutton/Download & set keyboard shortcuts.pushbutton/script.py
@@ -4,7 +4,22 @@ Copy file to your hardrive and click Import. Then find the downloaded file and c
 __context__ = 'zero-doc'
 
 import subprocess
-subprocess.Popen(r'explorer /select,"U:\REVIT\Keyboard_Shortcuts\KeyboardShortcuts_davidv.xml"')
+# getting %HOMEPATH%
+from os.path import expanduser
+homepath = expanduser("~")
+
+
+# path for common user
+try:
+    ksPath = homepath + "\AppData\Roaming\pyRevit\Extensions\CustomTools.extension\CustomTools.tab\Links.panel\Keyboard shortcuts.splitpushbutton\Download & set keyboard shortcuts.pushbutton\KeyboardShortcuts_davidv.xml"
+    test = open(ksPath,"r")
+# path for developer using git clone
+except:
+    ksPath = homepath + "\Documents\gitRepos\pyRevit extensions\CustomTools.extension\CustomTools.tab\Links.panel\Keyboard shortcuts.splitpushbutton\Download & set keyboard shortcuts.pushbutton\KeyboardShortcuts_davidv.xml"
+
+subprocess.Popen(r'explorer /select, '+ksPath)
+# subprocess.Popen(r'explorer /select,"U:\REVIT\Keyboard_Shortcuts\KeyboardShortcuts_davidv.xml"')
+
 
 #run command keyboard Shortcuts
 from Autodesk.Revit.UI import UIApplication, RevitCommandId, PostableCommand
--- CustomTools.tab/Links.panel/Keyboard shortcuts.splitpushbutton/Keyboard shortcuts.pushbutton/script.py
+++ CustomTools.tab/Links.panel/Keyboard shortcuts.splitpushbutton/Keyboard shortcuts.pushbutton/script.py
@@ -8,6 +8,6 @@ from pyrevit import script
 __context__ = 'zero-doc'
 __title__ = 'Keyboard\nshortuts'
 
-
-url = 'https://gfi.miraheze.org/wiki/Klávesové_skratky_-_Revit#Dávid_Vadkerti'
+url = 'https://airtable.com/shriLXgkKNvsD4O0X'
+# url = 'https://gfi.miraheze.org/wiki/Klávesové_skratky_-_Revit#Dávid_Vadkerti'
 script.open_url(url)
